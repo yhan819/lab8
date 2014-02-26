25e8e6613c80442467beee5026a64807c8f258b7
Yoon-Suk (John) Han <yhan819@stanford.edu>
2014-02-26 15:51:51 -0800

    added stretch goal to track time

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var starttime;
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -9,7 +10,14 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	starttime = new Date().getTime();
+	var clicked = false;
 	$(".likeBtn").click(function(e) {
+		var time = new Date().getTime(); - starttime;
 		ga('send','event','like','click');
+		if (!clicked) {
+			ga('send','timing','userAction','To Click', time, 'user');
+			clicked = true;
+		}
 	});
 }
